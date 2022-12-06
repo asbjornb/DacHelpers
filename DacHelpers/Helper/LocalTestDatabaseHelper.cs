31b04d4a3251e4a95a84d6f3f8b18a9b217d721c
Asbjørn Brandt <asbjoernbrandt@gmail.com>
2022-12-06 21:26:48 +0100

    Add implementation for local TestDatabaseHelper

---
+++ DacHelpers/Helper/LocalTestDatabaseHelper.cs
@@ -0,0 +1,13 @@
+﻿namespace DacHelpers.Helper;
+
+public class LocalTestDatabaseHelper : AbstractTestDatabaseHelper
+{
+    public LocalTestDatabaseHelper(string connectionString, string databaseName) : base(connectionString, databaseName)
+    {
+    }
+
+    public async override Task CleanUpAsync()
+    {
+        await DropDatabaseAsync();
+    }
+}

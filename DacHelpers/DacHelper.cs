83d0d16928346cafa4fe2de7b149582280223f6b
Asbjørn Brandt <asbjoernbrandt@gmail.com>
2022-12-06 23:04:48 +0100

    Add DacHelper

---
+++ DacHelpers/DacHelper.cs
@@ -0,0 +1,74 @@
+﻿using DacHelpers.Helper;
+using Microsoft.Build.Utilities;
+using Microsoft.Data.SqlClient;
+using Microsoft.SqlServer.Dac;
+
+namespace DacHelpers;
+
+public static class DacHelper
+{
+    /// <summary>
+    /// Deploys a DACPAC at the given path to localhost at the given database name
+    /// </summary>
+    /// <param name="dacpacPath">Path to the DACPAC</param>
+    /// <param name="databaseName">Name of the database to deploy to</param>
+    public static async Task<ITestDatabaseHelper> DropAndDeployLocal(string dacpacPath, string databaseName)
+    {
+        return await DropAndDeployLocal(dacpacPath, databaseName, new Dictionary<string, string>());
+    }
+
+    /// <summary>
+    /// Deploys a DACPAC at the given path to localhost at the given database name
+    /// </summary>
+    /// <param name="dacpacPath">Path to the DACPAC</param>
+    /// <param name="databaseName">Name of the database to deploy to</param>
+    /// <param name="sqlCmdVariables">SQLCMD variables to pass to the DACPAC</param>
+    public static async Task<ITestDatabaseHelper> DropAndDeployLocal(string dacpacPath, string databaseName, Dictionary<string, string> sqlCmdVariables)
+    {
+        var connectionString = GetConnectionStringLocal(databaseName);
+        await DropAndCreateDatabase(connectionString, databaseName);
+
+        var dacOptions = new DacDeployOptions
+        {
+            BlockOnPossibleDataLoss = false
+        };
+
+        foreach (var keyValuePair in sqlCmdVariables)
+        {
+            dacOptions.SqlCommandVariableValues.Add(keyValuePair);
+        }
+
+        var dacServiceInstance = new DacServices(connectionString);
+        //Could hook up here to dacServiceInstance.ProgressChanged and .Message to get progress updates but not sure where to log them
+        try
+        {
+            using DacPackage dacpac = DacPackage.Load(dacpacPath);
+            dacServiceInstance.Deploy(dacpac, databaseName
+                                    , upgradeExisting: true
+                                    , options: dacOptions
+                                    );
+        }
+        catch (Exception)
+        {
+            //How do we handle or log these?
+            throw;
+        }
+
+        return new LocalTestDatabaseHelper(connectionString, databaseName);
+    }
+
+    private static async System.Threading.Tasks.Task DropAndCreateDatabase(string connectionString, string databaseName)
+    {
+        using var connection = new SqlConnection(connectionString);
+        connection.Open();
+        var command = connection.CreateCommand();
+
+        command.CommandText = SqlQueryStrings.DropAndCreateDatabase(databaseName);
+        await command.ExecuteNonQueryAsync();
+    }
+
+    private static string GetConnectionStringLocal(string dbName)
+    {
+        return $"Data Source=localhost;Initial Catalog={dbName};Integrated Security=SSPI;";
+    }
+}
--- DacHelpers/SqlQueryStrings.cs
+++ DacHelpers/SqlQueryStrings.cs
@@ -4,11 +4,16 @@ public static class SqlQueryStrings
 {
     public static string DropDatabaseSql(string databaseName)
     {
-        return "USE master;" +
-              $"IF EXISTS (SELECT * FROM sys.databases WHERE name = '{databaseName}')" +
-               "BEGIN" +
-              $"ALTER DATABASE {databaseName} SET SINGLE_USER WITH ROLLBACK IMMEDIATE;" +
-              $"DROP DATABASE {databaseName};" +
-               "END";
+        return $"USE master;{Environment.NewLine}" +
+               $"IF EXISTS (SELECT * FROM sys.databases WHERE name = '{databaseName}'){Environment.NewLine}" +
+               $"BEGIN{Environment.NewLine}" +
+               $"ALTER DATABASE {databaseName} SET SINGLE_USER WITH ROLLBACK IMMEDIATE;{Environment.NewLine}" +
+               $"DROP DATABASE {databaseName};{Environment.NewLine}" +
+               $"END{Environment.NewLine}";
+    }
+
+    public static string DropAndCreateDatabase(string databaseName)
+    {
+        return DropDatabaseSql(databaseName) + $"CREATE DATABASE {databaseName};{Environment.NewLine}";
     }
 }

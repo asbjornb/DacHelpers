1bf7690e0d143371dfac809e5db00117feda0b9a
Asbjørn Brandt <asbjoernbrandt@gmail.com>
2022-12-10 15:40:28 +0100

    Assert that table is created

--- Test/DacHelpers.Test/LocalDatabaseTests.cs
+++ Test/DacHelpers.Test/LocalDatabaseTests.cs
@@ -21,7 +21,13 @@ public class LocalDatabaseTests
         var connectionString = testDatabaseHelper.ConnectionString;
 
         using var database = new Database(connectionString, "Microsoft.Data.SqlClient");
-        var result = await database.ExecuteScalarAsync<string>("SELECT @@@Version");
-        Assert.That(result, Does.Contain("Microsoft SQL Server"));
+        var version = await database.ExecuteScalarAsync<string>("SELECT @@@Version");
+        Assert.That(version, Does.Contain("Microsoft SQL Server"));
+        var rows = await database.FetchAsync<dynamic>("SELECT TABLE_CATALOG, TABLE_SCHEMA, TABLE_NAME FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME='Table1';");
+        Assert.That(rows, Has.Count.EqualTo(1));
+        var tableData = rows.Single();
+        Assert.That(tableData.TABLE_CATALOG, Is.EqualTo(databaseName));
+        Assert.That(tableData.TABLE_SCHEMA, Is.EqualTo("dbo"));
+        Assert.That(tableData.TABLE_NAME, Is.EqualTo("Table1"));
     }
 }

e4224f082e195045670fb9f234040ded2412ced1
Asbjørn Brandt <asbjoernbrandt@gmail.com>
2022-12-10 15:26:49 +0100

    Add first test

---
+++ Test/DacHelpers.Test/LocalDatabaseTests.cs
@@ -0,0 +1,27 @@
+﻿using PetaPoco;
+
+namespace DacHelpers.Test;
+
+[TestFixture]
+public class LocalDatabaseTests
+{
+    [OneTimeSetUp]
+    public void OneTimeSetUp()
+    {
+        var dir = Path.GetDirectoryName(new Uri(typeof(LocalDatabaseTests).Assembly.Location).LocalPath);
+        Directory.SetCurrentDirectory(dir!);
+    }
+
+    [Test]
+    public async Task ShouldDeployToLocalDatabase()
+    {
+        const string dacpacPath = "TestDatabase.dacpac";
+        const string databaseName = "TestDb";
+        var testDatabaseHelper = await DacHelper.DropAndDeployLocalAsync(dacpacPath, databaseName);
+        var connectionString = testDatabaseHelper.ConnectionString;
+
+        using var database = new Database(connectionString, "Microsoft.Data.SqlClient");
+        var result = await database.ExecuteScalarAsync<string>("SELECT @@@Version");
+        Assert.That(result, Does.Contain("Microsoft SQL Server"));
+    }
+}
---
+++ Test/TestDatabase/dbo/Tables/Table1.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Table1]
+(
+    [Id] int IDENTITY(1,1) NOT NULL,
+    [SomeString] nvarchar(50) NULL,
+    [SomeDate] date NULL,
+    CONSTRAINT [PK_Table1] PRIMARY KEY CLUSTERED ([Id] ASC)
+)
